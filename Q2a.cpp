0f6f45b144d1736f8bbda633994d5cbfa30b0286
Ben Leone <benleone90@gmail.com>
2019-10-19 14:16:50 -0400

    Q2a working

---
+++ Q2a.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+//Recursive function to find GCD of two positive integers
+int gcd(int m, int n){
+    if (m <= 0 || n <= 0){
+        cout << "Invalid input. Must be positive integers." << endl;
+    } else if (m % n == 0){
+        return n;
+    }
+    else {
+        return gcd (n, m % n);
+    }
+    return 0;
+}
+
+
+int main()
+{
+    int m = 0;
+    int n = 0;
+    while (m == 0 || n == 0){
+        cout << "Enter m: ";
+        cin >> m;
+        cout << "Enter n: ";
+        cin >> n;
+        cout << "GCD is: " << gcd (m, n) << endl;
+    }
+    return 0;
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # EC327_PA2
+Programming Assignment 2
